b25d25adab08d2d1ecdf0ad92e81e52d14623d1d
Kaupaunha <fannycoponat@gmail.com>
2022-10-04 18:04:18 +0200

    get API data

---
+++ front/js/index.js
@@ -0,0 +1,27 @@
+// Récupérer API
+fetch('http://localhost:3000/api/products')
+    .then(res => res.json())
+    .then(data => {
+        showProducts(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+
+// Afficher les produits
+function showProducts(data) {
+    for (product of data) {
+        const itemCard = document.getElementById('items');
+        itemCard.innerHTML += `
+        <a href="./product.html?id=${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+        </a>
+      `; 
+    }
+}
+
